b6186992bfcb7370ead7566884cd0f08bbdac66a
1jsingh <1.jaskiratsingh@gmail.com>
2018-12-12 15:22:12 +0000

    update

--- maddpg/maddpg.py
+++ maddpg/maddpg.py
@@ -3,7 +3,7 @@ import random
 import copy
 from collections import namedtuple, deque
 
-from model4 import Actor, Critic
+from model import Actor, Critic
 
 import torch
 import torch.nn.functional as F
